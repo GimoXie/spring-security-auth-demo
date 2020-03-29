ec0cf093de7833e822568ba5e16f886795011209
GimoXie <232571189@qq.com>
2020-03-29 21:44:08 +0800

    1.maven java10 plugin 2.jwt非对称加密

--- oauth2/server-authorization/src/main/java/io/gimo/auth/oauth2/server/authonrization/config/JwtConfig.java
+++ oauth2/server-authorization/src/main/java/io/gimo/auth/oauth2/server/authonrization/config/JwtConfig.java
@@ -2,10 +2,16 @@ package io.gimo.auth.oauth2.server.authonrization.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
+import java.security.KeyPair;
+
+// https://www.cnblogs.com/guos/p/11661972.html 非对称加密方式
 @Configuration
 public class JwtConfig {
 
@@ -17,7 +23,11 @@ public class JwtConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-        accessTokenConverter.setSigningKey("Gimo");
+        /* 对称加密实现
+        accessTokenConverter.setSigningKey("Gimo");*/
+        KeyStoreKeyFactory factory =  new KeyStoreKeyFactory(new ClassPathResource("oauth2.jks"), "oauth2".toCharArray());
+        KeyPair keyPair = factory.getKeyPair("oauth2");
+        accessTokenConverter.setKeyPair(keyPair);
         return accessTokenConverter;
     }
 }
--- oauth2/server-resource/src/main/java/io/gimo/oauth2/server/resource/config/JwtConfig.java
+++ oauth2/server-resource/src/main/java/io/gimo/oauth2/server/resource/config/JwtConfig.java
@@ -2,9 +2,16 @@ package io.gimo.oauth2.server.resource.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 @Configuration
 public class JwtConfig {
@@ -17,7 +24,16 @@ public class JwtConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-        accessTokenConverter.setSigningKey("Gimo");
+        /* 对称加密方式
+        accessTokenConverter.setSigningKey("Gimo");*/
+        Resource resource = new ClassPathResource("pub.key");
+        String publicKey = null;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            publicKey = br.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        accessTokenConverter.setSigningKey(publicKey);
         return accessTokenConverter;
     }
 }
